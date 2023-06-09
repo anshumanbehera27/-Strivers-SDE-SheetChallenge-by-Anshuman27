3a6f7726586f3580e2dbad2ee121411abd0babae
Anshuman behera <90245492+anshumanbehera27@users.noreply.github.com>
2023-06-09 22:26:28 +0530

    Add files via upload

---
+++ BinarySearch/SearchOnThe2dMatrix.java
@@ -0,0 +1,23 @@
+package binaryRev;
+
+public class SearchOnThe2dMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int start = 0 ;
+        int end = (n*m) -1 ;
+        while (start <= end){
+            int mid = start +(end -start) / 2 ;
+
+            // check the paticular index by mid/m and mid % m
+            if (matrix[mid/m][mid%m] == target) return  true ;
+            else if (matrix[mid/m][mid%m] < target)  start = mid + 1 ;
+
+            else end = mid -1 ;
+
+        }
+        return false;
+
+    }
+}
